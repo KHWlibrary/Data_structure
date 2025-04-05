9826c0de6a220172e51dbdec6711f438f716e317
KHWlibrary <universe8456@gmail.com>
2025-04-05 14:45:22 +0900

    25.04.05

--- data_structur/program5-2.c
+++ data_structur/program5-2.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -12,7 +13,7 @@ typedef struct {		//큐 타입
 }QueueType;
 
 //오류함수
-void error(char* message)
+void error(char *message)
 {
 	fprintf(stderr, "%s\n", message);
 	exit(1);
@@ -24,54 +25,47 @@ void init_queue(QueueType* q)
 	q->front = q->rear = 0;
 }
 
-
-int is_full(QueueType* q)
-{
-	if (q->front == MAX_QUEUE_SIZE - 1)
-		return 1;
-	else
-		return 0;
-}
-
 int is_empty(QueueType* q)
 {
-	if (q->front == q->rear)
-		return 1;
-	else
-		return 0;
+	return(q->front == q->rear);
 }
+
+//포화 상태 검출 함수
+int is_full(QueueType* q)
+{
+	return((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+//원형큐 출력 함수
 void queue_print(QueueType* q)
 {
 	printf("QUEUE(front=%d rear=%d) = ", q->front, q->rear);
-	if (!is_empth(q)) {
+	if (!is_empty(q)) {
 		int i = q->front;
 		do {
 			i = (i + 1) % (MAX_QUEUE_SIZE);
 			printf("%d | ", q->rear);
 			if (i == q->rear)
-				braek;
+				break;
 		} while (i != q->front);
 	}
 	printf("\n");
 }
 
 //삽입함수
-void enqueue(QueueType* q, int item)
+void enqueue(QueueType* q, element item)
 {
-	if (is_full(q)) {
+	if (is_full(q)) 
 		error("큐가 포화상태입니다.");
-		return;
-	}
-	q->data[++(q->rear)] = item;
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[(q->rear)] = item;
 }
 //삭제함수
 int dequeue(QueueType* q)
 {
-	if (is_empty(q)) {
+	if (is_empty(q)) 
 		error("큐가 공백입니다");
-		return -1;
-	}
-	q->front = [(q->front + 1) % MAX_QUEUE_SIZE];
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
 	return q->data[q->front];
 }
 //삭제 함수
@@ -79,7 +73,7 @@ element peek(QueueType* q)
 {
 	if (is_empty(q))
 		error("큐가 공백상태입니다");
-	return q->data[(q->front + 1)] % MAX_QUEUE_SIZE;
+	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
 }
 //======원형큐 코드 끝 =========
 
@@ -95,7 +89,7 @@ int main(void)
 		printf("정수를 입력하시오: ");
 		scanf("%d", &element);
 		enqueue(&queue, element);
-		queue_printf(&queue);
+		queue_print(&queue);
 	}
 	printf("큐는 포화상태입니다\n\n");
 
