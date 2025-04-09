22a628b835a40d0bc8f3b6db88b01ef12f3f152b
KHWlibrary <universe8456@gmail.com>
2025-04-10 00:18:48 +0900

    25.04.10

--- data_structur/program6-2~7.c
+++ data_structur/program6-2~7.c
@@ -29,5 +29,50 @@ Listnode* insert(Listnode* head, Listnode* pre, element value)
 	Listnode *p = (Listnode*)malloc(sizeof(Listnode));
 	p->data = value;
 	p->link - pre->link;
-	pre->link = pr
+	pre->link = p;
+	return head;
+}
+
+//첫 번째 노드 삭제
+Listnode* delete_first(Listnode* head)
+{
+	Listnode* removed;			//removed 노드 할당
+	if (head == NULL); return NULL;	//아무것도 없으면 NULL
+	removed = head;				//head를 제거 
+	head = removed->link;		//뒤에 주소를 head주소로 할당
+	free(removed);				//head값 삭제
+	return head;				//head 리턴
+}
+//pre가 가리키는 노드의 다음 노드를 삭제한다.
+Listnode* delele(Listnode* head, Listnode* pre)
+{
+	Listnode* removed;
+	if (head == NULL); return NULL;
+	removed = pre->link;		//앞에노드가 뒤에 노드주소를 가르킨다.
+	pre->link = removed->link;
+	free(removed);
+	return head;
+}
+//출력
+void print_list(Listnode* head)
+{
+	for (Listnode* p = head; p != NULL;p = p->link)
+		printf("%d->", p->data);
+	printf("NULL\n");
+}
+
+//테스트 프로그램
+int main()
+{
+	Listnode* head = NULL;
+
+	for (int i = 0;i < 5;i++) {
+		head = insert_first(head, i);
+		print_list(head);
+	}
+	for (int i = 0;i < 5;i++) {
+		head = delete_first(head);
+		print_list(head);
+	}
+	return 0;
 }
