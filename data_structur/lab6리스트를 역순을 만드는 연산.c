06c2b62fb2484c7bbb0cbae9ac220fb966a2ba43
KHWlibrary <universe8456@gmail.com>
2025-04-13 22:01:16 +0900

    25.04.13

--- data_structur/lab6리스트를 역순을 만드는 연산.c
+++ data_structur/lab6리스트를 역순을 만드는 연산.c
@@ -27,3 +27,36 @@ void print_list(ListNode* head)
 	}
 	printf("NULL\n");  // 리스트의 끝을 나타내는 NULL 출력
 }
+//리스트 역순으로 만들기
+ListNode* reverse(ListNode* head)
+{
+	//순회 포인터로 p, q, r을 사용
+	ListNode* p, * q, * r;
+
+	p = head;		//p는 역순으로 만들 리스트
+	q = NULL;		//q는 역순을 만들 노드
+	while (p != NULL) {
+		r = q;	//r은 역순이 된 리스트.
+				//r은 q,q는 p를 차례로 따라간다.
+		q = p;
+		p = p->link;
+		q->link = r;	//q의 링크 방향을 바꾼다
+	}
+	return q;
+}
+
+//테스트 프로그램
+int main(void)
+{
+	ListNode* head1 = NULL;
+	ListNode* head2 = NULL;
+
+	head1 = insert_first(head1, 10);
+	head1 = insert_first(head1, 20);
+	head1 = insert_first(head1, 30);
+	print_list(head1);
+
+	head2 = reverse(head1);
+	print_list(head2);
+	return 0;
+}
